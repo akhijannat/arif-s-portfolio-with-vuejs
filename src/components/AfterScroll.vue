914119256e272ffada000197ebbe0b790c4e64fd
akhi jannat <ajjannat02@gmail.com>
2023-09-17 12:44:32 +0600

    navigation active update

--- src/components/AfterScroll.vue
+++ src/components/AfterScroll.vue
@@ -1,65 +1,134 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from "vue";
 
-const navLink =
-  "flex justify-end items-center space-x-2 py-[3px] inline-block transition duration-1000";
-const navDot = "h-3 w-3 rounded-full bg-red-700 transition duration-1000";
-const navText =
-  "uppercase opacity-0 text-[12px] font-bold  transition duration-1000";
-
 const isVisible = ref(false);
+const activeItem = ref(null);
 
 const handleScroll = () => {
   const scrollPosition = window.scrollY;
   const screenHeight = window.innerHeight;
-
-  // Calculate how far the user has scrolled in screens
   const screensScrolled = scrollPosition / screenHeight;
-
-  // Check if the user has scrolled at least one screen
   isVisible.value = screensScrolled >= 1;
+
+  const categoryPositions = {
+    intro: 1,
+    services: 2,
+    skills: 3,
+    workExample: 4,
+    clientExperience: 5,
+    contact: 6,
+  };
+
+  // Determine which category is currently in view
+  let currentCategory = null;
+  for (const category in categoryPositions) {
+    if (scrollPosition >= categoryPositions[category] * screenHeight) {
+      currentCategory = category;
+    }
+  }
+
+  // Map the current category to the corresponding navigation item
+  const categoryToItem = {
+    intro: 0,
+    services: 1,
+    skills: 2,
+    workExample: 3,
+    clientExperience: 4,
+    contact: 5,
+  };
+
+  // Update the active item based on the current category
+  if (categoryToItem[currentCategory] !== undefined) {
+    activeItem.value = categoryToItem[currentCategory];
+  }
 };
 
-// Attach scroll event listener when the component is mounted
+const handleItemClick = (itemNumber) => {
+  activeItem.value = itemNumber;
+};
+
 onMounted(() => {
   window.addEventListener("scroll", handleScroll);
 });
 
-// Remove scroll event listener when the component is unmounted
 onUnmounted(() => {
   window.removeEventListener("scroll", handleScroll);
 });
 </script>
 <template>
   <!-- Navigation -->
-  <nav v-if="isVisible" class="fixed right-8 top-1/3 text-right">
-    <a href="#intro" :class="navLink" class="nav-item">
-      <p :class="navText">Intro</p>
-      <div :class="navDot"></div>
-    </a>
-    <a href="#services" :class="navLink" class="nav-item">
-      <p :class="navText">What I Do</p>
-      <div :class="navDot"></div>
-    </a>
-    <a href="#skills" :class="navLink" class="nav-item">
-      <p :class="navText">Skills</p>
-      <div :class="navDot"></div>
-    </a>
+  <nav
+    v-if="isVisible"
+    id="dot-nav"
+    class="fixed z-[1] top-1/2 right-8 bottom-auto translate-y-[-50%] transition-opacity visible"
+  >
+    <div class="text-right">
+      <a
+        href="#intro"
+        data-number="0"
+        class=""
+        @click="handleItemClick(0)"
+        :class="{ navLink, 'active-item': activeItem === 0 }"
+      >
+        <span class="dot"></span>
+        <span class="dot-label">Intro</span>
+      </a>
 
-    <a href="#workExample" :class="navLink" class="nav-item">
-      <p :class="navText">Work Example</p>
-      <div :class="navDot"></div>
-    </a>
+      <a
+        href="#services"
+        data-number="1"
+        class=""
+        @click="handleItemClick(1)"
+        :class="{ navLink, 'active-item': activeItem === 1 }"
+      >
+        <span class="dot"></span>
+        <span class="dot-label">What I Do</span>
+      </a>
 
-    <a href="#clientExperience" :class="navLink" class="nav-item">
-      <p :class="navText">Client Experience</p>
-      <div :class="navDot"></div>
-    </a>
+      <a
+        href="#skills"
+        data-number="2"
+        class=""
+        @click="handleItemClick(2)"
+        :class="{ navLink, 'active-item': activeItem === 2 }"
+      >
+        <span class="dot"></span>
+        <span class="dot-label">Skills</span>
+      </a>
 
-    <a href="#contact" :class="navLink" class="nav-item">
-      <p :class="navText">Contact</p>
-      <div :class="navDot"></div>
-    </a>
+      <a
+        href="#workExample"
+        data-number="3"
+        class=""
+        @click="handleItemClick(3)"
+        :class="{ navLink, 'active-item': activeItem === 3 }"
+      >
+        <span class="dot"></span>
+        <span class="dot-label">Work Example</span>
+      </a>
+
+      <a
+        href="#clientExperience"
+        data-number="4"
+        class=""
+        @click="handleItemClick(4)"
+        :class="{ navLink, 'active-item': activeItem === 4 }"
+      >
+        <span class="dot"></span>
+        <span class="dot-label">Client Experience</span>
+      </a>
+
+      <a
+        href="#contact"
+        data-number="5"
+        class=""
+        @click="handleItemClick(5)"
+        :class="{ navLink, 'active-item': activeItem === 5 }"
+      >
+        <span class="dot"></span>
+        <span class="dot-label">Contact</span>
+      </a>
+    </div>
   </nav>
 
   <!-- Back to top Button -->
@@ -83,14 +152,45 @@ onUnmounted(() => {
 </template>
 
 <style scoped>
-.nav-item:hover p {
-  color: black;
+#dot-nav a::after {
+  clear: both;
+  content: "";
+  display: table;
+}
+
+#dot-nav a span {
+  float: right;
+  display: inline-block;
+  transform: scale(0.6);
+}
+
+#dot-nav a:hover span {
+  transform: scale(0.75);
+}
+
+#dot-nav .dot {
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+  background-color: #ff0000;
+  transition: transform 0.2s, background-color 0.5s;
+  transform-origin: 50% 50%;
+}
+
+#dot-nav .dot-label {
+  padding-right: 5px;
+  color: #000;
+  font-size: 17px;
+  font-weight: 500;
+  text-transform: uppercase;
+  opacity: 0;
+  transition: transform 0.2s, opacity 0.2s;
+  transform-origin: 100% 50%;
+}
+#dot-nav a:hover .dot-label {
   opacity: 1;
 }
-.nav-item:hover div {
-  background-color: black;
-}
-.is-selected div {
-  background-color: black;
+#dot-nav a.active-item .dot {
+  background-color: green;
 }
 </style>
